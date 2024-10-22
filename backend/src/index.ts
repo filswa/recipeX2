ad9c3debd0ed8a53c83c7229f583b978cb78fde6
Filip Swacha <filip.swacha@gmail.com>
2024-10-22 22:26:52 +0200

    add dev-tools:
    - add babel
    - add jest
    - add dummy routes on the server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,25 +3,77 @@ import express, { Request, Response } from "express";
 // Create a new express application instance
 const app = express();
 
+// Set the network port
+const port = process.env.PORT || 3000;
+
 const cors = { origin: ["http://localhost:5173/"], default: '*' }
   
 app.all('*', function(req:any, res, next) {   
     if(!req.header){
         throw new Error('Request header undefined')
-        return;
     }
+
     const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-// Set the network port
-const port = process.env.PORT || 3000;
-
 // Define the root path with a greeting message
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (_req: Request, res: Response) => {
     res.json({ message: "Welcome to the Express + TypeScript Server!" });
+});
+
+app.post("/signup", (req: Request, res: Response) => {
+    res.json({ message: "account created"})
+});
+
+app.post("/login", (req: Request, res: Response) => {
+    res.json({message: "logged in"})
+});
+
+app.get("/user", (req: Request, res: Response) => {
+    res.json({
+        user:{
+            id: 1,
+            username: "Filip",
+            email: "filip.swacha.dev@gmail.com",
+            password: "password",
+            settings: {
+                theme: "dark",
+                language: "en"
+            },
+        }
+    })
+});
+
+app.get("/recipes", (req: Request, res: Response) => {
+    res.json({
+        recipes: [
+            {
+                title: "Recipe 1",
+                description: "Description 1",
+                tags: ["sweet", "awesome", "party"],
+                servings: 1,
+                imgSrc: "someImageUrl",
+                ingredients: [
+                    ["rum", 100, "ml"],
+                    ["orange juice", 100, "ml"],
+                    ["banana juice", 100, "ml"],
+                    ["apple", 1, ""]
+                ],
+            }
+        ]
+    });
+})
+
+app.patch("/user/:userId", (req: Request, res: Response) => {
+    
+});
+
+app.delete("/user/:userId", (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    res.json({message: `user ${userId} deleted`});
 });
 
 // Start the Express server
